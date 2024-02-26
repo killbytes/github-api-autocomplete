7606085d727fce51e2d3e2227398f5e0a7672b84
killcodings <kontaktid@ya.ru>
2024-02-27 01:30:59 +0300

    feat: add check error fetch and id and trim input

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const searchBlock = document.querySelector('.search-block');
 const resultBlock = document.querySelector('.result-list');
 const searchList = document.querySelector('.list');
 
-let repositaries = [];
+let repositories = [];
 
 async function searchRep() {
-    if (input.value) {
+    if (input.value.trim() !== '') {
         return await fetch(`https://api.github.com/search/repositories?q=${input.value}&per_page=${5}`)
             .then(response => {
-                response.json().then(listResult)
+                if(response.ok) {
+                    response.json().then(listResult)
+                } else {
+                    throw new Error('Error response, try correct input value again');
+                }
             })
     } else {
         clearSearchList(); // if input.value empty
@@ -26,32 +30,33 @@ function deBounce(func, timeout) {
     };
 }
 
-function createElem(tag, classTag, text) {
+function createElem(tag, classTag, text, id = null) {
     const el = document.createElement(tag);
     if (classTag) el.classList.add(classTag);
     if (text) el.textContent = text;
+    if (id) el.id = id;
     return el;
 }
 
 function listResult(response) {
     if (input.value) {
         clearSearchList();
-        response.items.forEach((e) => {
-            let elem = createElem('li', 'list__item', e['name']);
+        response.items.forEach((e, id) => {
+            let elem = createElem('li', 'list__item', e['name'], e['id']);
             searchList.append(elem);
-            repositaries.push(e);
+            repositories.push(e);
         });
-        console.log(repositaries);
+        console.log(repositories);
     }
 }
 
 function clearSearchList() {
     searchList.innerHTML = '';
-    repositaries = [];
+    repositories = [];
 }
 
 function selectRep(repos) {
-    let liResItem = createElem('li', 'result-list__item');
+    let liResItem = createElem('li', 'result-list__item', '', repos['id']);
     let infoList = createElem('ul', 'info-list');
     let elemName = createElem('li', 'items', `Name: ${repos['name']}`);
     infoList.append(elemName);
@@ -68,8 +73,8 @@ function selectRep(repos) {
 input.addEventListener('keyup', deBounce(searchRep, 600));
 
 searchBlock.addEventListener('click', (event) => {
-    repositaries.forEach((e) => {
-        if (event.target.textContent === e['name']) {
+    repositories.forEach((e) => {
+        if (event.target.id == e['id']) {
             selectRep(e); // add card result
             input.value = '';
             clearSearchList(); // clear search list
@@ -78,7 +83,7 @@ searchBlock.addEventListener('click', (event) => {
 })
 
 resultBlock.addEventListener('click', function (event) { // close result card
-    if (event.target.classList[0] === 'close-button') {
+    if (event.target.closest('.close-button') || event.target.classList.contains('close-button')) {
         event.target.parentElement.remove();
     }
 })
